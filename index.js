474376c4f7f38174d828b038b943c1fbe6f09a82
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-09-04 11:52:39 -0300

    adicionado botao para envio dos dados

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const jantar = document.getElementById('jantar').value;
+const jantar = document.getElementById('jantar');
 const taxa = document.getElementById('taxa').value;
 const pessoas = document.getElementById('pessoas').value;
+const botao = document.querySelector('button');
 const valorPessoa = document.getElementById('valorPessoa');
+
+botao.addEventListener('click', somarJantarTaxa);
 
 function somarJantarTaxa(){
     let soma = jantar + taxa;
     console.log(soma);
 }
-
-somarJantarTaxa();
